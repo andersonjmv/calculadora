50ac44cddc35ae3cdf561adb0d38ee133b1bc38e
andersonjmv <anderson.manosalva@gmail.com>
2021-08-25 21:44:26 -0500

    calculadora final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { num, ops, ids } from './components/IdBotones'
+import { ViewCalculator } from './components/ViewCalculator'
 
 function App() {
+
+  const [state, setState] = React.useState({
+    lastPressed: undefined,
+    calc: '0',
+    operation: undefined
+  })
+
+  const handleClick = (e) => {
+    let { calc, lastPressed } = state
+    const { innerText } = e.target
+
+    switch (innerText) {
+      case 'AC':
+        {
+          setState({
+            ...state,
+            calc: '0',
+            lastPressed: innerText
+          })
+          break;
+        }
+      case '=': {
+        const evaluated = eval(calc)
+        setState({
+          ...state,
+          calc: evaluated,
+          lastPressed: innerText
+        })
+        break;
+      }
+      case '.': {
+        const splitted = calc.split(/[\+\-\*\/]/)
+        const last = splitted.slice(-1)[0];
+
+        if (!last.includes('.')) {
+          setState({
+            ...state,
+            calc: calc + '.',
+
+          })
+        }
+
+        break
+      }
+      default: {
+        let e = undefined;
+        // check operations
+        if (ops.includes(innerText)) {
+          if (ops.includes(lastPressed) && innerText !== '-') {
+            const lastNumberIdx = calc.split('').reverse().
+              findIndex(char => char !== ' ' && num.includes(+char))
+            // console.log(calc.split(''),lastNumberIdx);
+            e = calc.slice(0, calc.length - lastNumberIdx) + ` ${innerText} `;
+            //console.log(e);
+          } else {
+            e = `${calc} ${innerText} `;
+          }
+        } else {
+          //console.log("numero");
+          e = (calc === '0') ? innerText : (calc + innerText)
+        }
+        setState({
+          ...state,
+          calc: e,
+          lastPressed: innerText
+        })
+      }
+    }
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1
+        className="text-center mt-5 p-2"
+      >Calculadora
+    </h1>
+      <ViewCalculator
+        handleClick={handleClick}
+        state={state}
+        num={num}
+        ops={ops}
+        ids={ids}
+      />
+    </>
   );
 }
-
 export default App;
